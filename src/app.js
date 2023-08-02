13aa08b21bac593de2d94a5473f406835848e775
NASTIIA0412 <anastasiianikitenko@gmail.com>
2023-08-02 16:36:07 +0300

    Showing the current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-alert("Hello");
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+let apiKey = "28b92728dc531dbd191e490122b175e2";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=dusseldorf&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
