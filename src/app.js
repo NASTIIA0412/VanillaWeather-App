74dd8a7c01a94de2b1e26aad9f7aee9564f680b9
NASTIIA0412 <anastasiianikitenko@gmail.com>
2023-08-02 22:09:15 +0300

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -44,8 +44,20 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let apiKey = "28b92728dc531dbd191e490122b175e2";
-let city = "dusseldorf";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "28b92728dc531dbd191e490122b175e2";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Kyiv");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
